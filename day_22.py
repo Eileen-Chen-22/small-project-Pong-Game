9c8ac7f4563c013d1b32a54d2a9fc92345e6b112
shichang ye <eileenchen20@outlook.com>
2022-08-31 10:58:50 -0700

    Finish the board

---
+++ day_22.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+from paddle_22 import Paddle
+from ball_22 import Ball
+from scoreboard_22 import ScoreBoard
+import time
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((360, 0))
+l_paddle = Paddle((-360, 0))
+ball = Ball()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 50 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+screen.exitonclick()
